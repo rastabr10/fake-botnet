4cd2edae0074b981433d090f1b6939b819628ca7
Juan <66240184+rastacabeza@users.noreply.github.com>
2021-04-25 13:00:41 -0300

    Add files via upload

---
+++ FakeBotnet.bat
@@ -0,0 +1,19 @@
+@echo off
+color 0d
+title Minecraft Kids Conected: 653
+echo.
+echo                          $$$$$$     $$$     $$$$$   $$$$$$    $$$   
+echo                          $$   $$   $$ $$   $$   $$    $$     $$ $$  
+echo                          $$   $$  $$   $$   $$$       $$    $$   $$ 
+echo                          $$$$$$   $$$$$$$     $$$     $$    $$$$$$$ 
+echo                          $$   $$  $$   $$  $$   $$    $$    $$   $$ 
+echo                          $$   $$  $$   $$   $$$$$     $$    $$   $$ 
+echo.
+echo.
+goto bienvenido
+
+:bienvenido
+echo Bienvenido a la botnet de RASTA el lammer.
+set/p pregunta= ~$
+
+pause
